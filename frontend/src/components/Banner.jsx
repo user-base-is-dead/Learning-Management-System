a543628dc92d42e4a3b375b0f81fee36f24ef043
Mishra <debasritmishra8@gmail.com>
2026-01-21 02:43:53 +0530

    Initial Commit

--- frontend/src/components/Banner.jsx
+++ frontend/src/components/Banner.jsx
@@ -1,9 +1,12 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { bannerStyles, customStyles } from "../assets/dummyStyles";
-import { floatingIcons } from "../assets/dummyBanner"
-import { Sparkle } from "lucide-react";
+import { features, floatingIcons } from "../assets/dummyBanner"
+import { Circle, CircleCheck, CircleCheckBig, Sparkle } from "lucide-react";
+import bannerImg from "../assets/Bannerimage.jpg"
+import video from "../assets/BannerVideo.mp4"
 
 const Banner = () => {
+  const [showVideo, setShowVideo] = useState(false);
   return (
     <div className={bannerStyles.container}>
               {/* Floating Icons Wrapper */}
@@ -33,9 +36,57 @@ const Banner = () => {
                     <span className={bannerStyles.headingSpan1}>Build Amazing</span>
                     <span className={bannerStyles.headingSpan2}>Digital Product</span>
                 </h1>
+
+                <p className={bannerStyles.description}>Create beautiful, responsive web applications with our powerful tools and components. Start building your next project today.</p>
+                {/* features */}
+                <div className={bannerStyles.featuresGrid}>
+                  {features.map((feature, i) => (
+                    <div key={i} className={bannerStyles.featureItem}>
+                      <div className={bannerStyles.featureIconContainer}>
+                        <span className={`${bannerStyles.featureIcon} text-${feature.color}-500`}>
+                          <CircleCheckBig size={16}/>
+                        </span>
+                      </div>
+                      <span className={bannerStyles.featureText}>
+                        {feature.text}
+                      </span>
+                    </div>
+                  ))}
+                </div>
+                {/* buttons */}
+                <div className={bannerStyles.buttonsContainer}>
+                  <a href="/courses" className={bannerStyles.buttonGetStarted}>Get Started</a>
+                  <button className={bannerStyles.buttonViewDemo} 
+                  onClick={() => setShowVideo(true)}>
+                    View Demo
+                    </button>
+                </div>
+            </div>
+            <div className={bannerStyles.imageContainer}>
+              <img src={bannerImg} alt="bannerimg" className={bannerStyles.image} />
             </div>
         </div>
       </div>
+      {/* video model */}
+      {showVideo && (
+        <div className={bannerStyles.videoModal.overlay}>
+          <div className={bannerStyles.videoModal.container}>
+            <iframe src={video} 
+            className={bannerStyles.videoModal.iframe} 
+            title="Demo Video" 
+            allow="auto-play; encrypted-media"
+            allowFullScreen>
+
+            </iframe>
+            <button onClick={() => setShowVideo(false)} className={bannerStyles.videoModal.closeButton}>
+              <span>
+                <X className={bannerStyles.videoModal.closeIcon}/>
+              </span>
+            </button>
+          </div>
+        </div>
+      )}
+      <style jsx>{customStyles}</style>
     </div>
   )
 }
