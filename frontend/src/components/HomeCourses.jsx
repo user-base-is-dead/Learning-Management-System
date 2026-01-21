8e1b6f484a4fde58f30839d44380981bd4cb7053
Mishra <debasritmishra8@gmail.com>
2026-01-21 12:45:35 +0530

    Initial Commit

--- frontend/src/components/HomeCourses.jsx
+++ frontend/src/components/HomeCourses.jsx
@@ -1,6 +1,11 @@
 import React from 'react'
+import { homeCoursesStyles } from "../assets/dummyStyles"
+import { coursesData } from "../assets/dummyHdata"
 
 const HomeCourses = () => {
+    const navigate = useNavigate();
+    const {title, course: courseFont, detail} = homeCoursesStyles.fonts;
+    const visibleCourse = coursesData.slice(0, 8);
   return (
     <div>HomeCourses</div>
   )
