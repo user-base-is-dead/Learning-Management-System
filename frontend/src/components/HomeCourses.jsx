4b2a1e7d64a625acf38666074fda95a6e4c2c44e
Mishra <debasritmishra8@gmail.com>
2026-01-21 12:51:58 +0530

    Initial Commit

--- frontend/src/components/HomeCourses.jsx
+++ frontend/src/components/HomeCourses.jsx
@@ -1,13 +1,24 @@
 import React from 'react'
 import { homeCoursesStyles } from "../assets/dummyStyles"
 import { coursesData } from "../assets/dummyHdata"
+import { Star } from "lucide-react";
 
 const HomeCourses = () => {
     const navigate = useNavigate();
     const {title, course: courseFont, detail} = homeCoursesStyles.fonts;
     const visibleCourse = coursesData.slice(0, 8);
   return (
-    <div>HomeCourses</div>
+    <div className={homeCoursesStyles.container}>
+        <div className={homeCoursesStyles.mainContainer}>
+            <div className={homeCoursesStyles.header}>
+                <h2 className={`${title} ${homeCoursesStyles.title}`}>
+                <Star className={homeCoursesStyles.titleIcon}/>
+                Explore Top Courses
+                <Star className={homeCoursesStyles.titleIcon}/>
+                </h2>
+            </div>
+        </div>
+    </div>
   )
 }
 
